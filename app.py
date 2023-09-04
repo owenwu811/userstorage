9a03c8caf662c07686393ae4b50f5056a97b9b09
owenwu811 <owu3231@gmail.com>
2023-09-03 21:02:04 -0700

    workingonemailfeature

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, session 
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 from db import db, User  # assuming your app's filename is 'app.py'
-import os
+from token_utils import generate_reset_token, verify_reset_token
+import os, smtplib
+smtplib.SMTP.debuglevel = 1
 
 app = Flask(__name__)
 
@@ -12,10 +15,20 @@ if environment == 'test':
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user_credentials.db'
 
+app.config['MAIL_SERVER'] = 'smtp.mail.yahoo.com'
+app.config['MAIL_PORT'] = 587 # commonly used port for sending mail
+app.config['MAIL_USERNAME'] = 'wuowen681@yahoo.com'
+app.config['MAIL_PASSWORD'] = 'Holo-light1234!'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
+
+
 app.secret_key =  '.'
 db.init_app(app)
 
-@app.route('/') #this is called a decorator in Python
+@app.route('/') 
 def welcome():
     return render_template('welcome.html')
 
@@ -27,13 +40,13 @@ def login():
         if user and user.password == password:
             print('Login successful')
             session['username'] = username
-            return redirect(url_for('landing'))  # Redirect to the landing page
+            return redirect(url_for('landing'))  
         return 'Login failed'
     return render_template('login.html')
 
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
-    session.pop('username', None)  # Remove the username from the session
+    session.pop('username', None)  
     return redirect(url_for('login'))
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -45,7 +58,6 @@ def register():
             return "Username already exists."
         if password != confirm_password:
             return "Passwords do not match."
-        # Store user data in the dictionary (insecure for demonstration purposes)
         new_user = User(username=username, password=password)
         db.session.add(new_user)
         db.session.commit()
@@ -59,6 +71,51 @@ def landing():
         return render_template('landing.html', username=username)
     else:
         return redirect(url_for('login'))
+    
+@app.route('/forgot_password', methods=['GET', 'POST'])
+def forgot_password():
+    if request.method == 'POST':
+        email = request.form['email']
+        token = generate_reset_token(email)
+        send_reset_email(email, token)
+        return "An email has been sent with instructions to reset your password."
+    return render_template('forgot_password.html')
+
+@app.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_password(token):
+    email = verify_reset_token(token)
+    if not email:
+        return 'Invalid or expired token', 400
+    if request.method == 'POST':
+        # Implement logic to reset the password
+        ...
+    return render_template('reset_password.html')
+
+def send_reset_email(email, token):
+    with app.app_context():
+        msg = Message('Password Reset Request',
+                      sender='noreply@yourdomain.com',
+                      recipients=[email])
+        msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_password', token=token, _external=True)}
+
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+        mail.send(msg)
+
+@app.route('/list_tables')
+def list_tables():
+    with app.app_context():
+        table_names = db.engine.table_names()
+        print("Database Tables:", table_names)
+    return "Check the console for the list of tables."
+
+@app.route('/list_users')
+def list_users():
+    users = User.query.all()
+    for user in users:
+        print(user.username, user.password)
+    return "Check the console for the list of users."
 
 with app.app_context():
     db.create_all()
@@ -66,9 +123,19 @@ with app.app_context():
 if __name__ == '__main__':
     app.run(debug=True)
 
+#email feature not working 
+#after making email feature work, write unittest for it
+
 #already did:
 #1. login + logout + register feature + database integration
 #2. wrote test for registration and login that worked with testlogin.py and testregistration.py
+#3. added list_tables and list_users routes to list all tables and list all usernames and passwords from the database
+#4. fixed the database persistance issue where running python3 testlogin.py would delete all usernames and passwords from database - solution was #bug fix solution for testlogin.py overriding database;
+# export MY_APP_ENV=test; python3 testlogin.py; flask run - a won't be able to login
+# unset MY_APP_ENV; flask run - a will still be able to login
+# 5. managed to create a feature branch on git so that you can rollback changes in case new features break already existing features or unittests - very important
+
+#5. fixed issue where testregistration unittest would fail after executing testlogin.py unit test first by generating unique data everytime the test is run so that you don't get already registered user by using unique_username = "testuser_" + str(uuid.uuid4()) - more in test_valid_registration function in testregistration.py
 
 #2. bug: database sometimes fails to save usernames after shutting down app - it only happens after I run the testlogin.py unittest because there is a db.drop_all() - use seperate database for testing?
 #0. add feature where users can't enter the wrong password more than 10 times
--- testregistration.py
+++ testregistration.py
@@ -1,10 +1,11 @@
 import unittest
+import uuid  # Import the uuid library
 from app import app, db, User  # Adjust the imports as per your setup
 
 class AuthTestCase(unittest.TestCase):
 
     def setUp(self):
-    # Set up the test client
+        # Set up the test client
         self.client = app.test_client()
         app.config['TESTING'] = True
 
@@ -19,9 +20,10 @@ class AuthTestCase(unittest.TestCase):
         ), follow_redirects=True)
     
     def test_valid_registration(self):
-        response = self.register('testuser', 'testpassword', 'testpassword')
+        # Use uuid to generate a unique username
+        unique_username = "testuser_" + str(uuid.uuid4())
+        response = self.register(unique_username, 'testpassword', 'testpassword')
         self.assertIn(b'Login', response.data)
-
 
 if __name__ == "__main__":
     unittest.main()
