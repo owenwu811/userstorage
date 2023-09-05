b0018583804cb268c2046761a1d77934b5ef8b87
owenwu811 <owu3231@gmail.com>
2023-09-04 18:59:48 -0700

    jenkinstest

--- allpytests.sh
+++ allpytests.sh
@@ -1,7 +1,9 @@
+#these tests are supposed to run everytime a new feature is added
 bash persistentlogintest.sh
 python3 testregistration.py
 python3 testlogout.py
 python3 testroute.py
 python3 testlogin.py
+python3 test10triesout.py
 
 
